00f00d22934b9414eaf6cb8e4d395a07ea20367a
gitApurv <apurvmaurya5@gmail.com>
2025-04-29 02:52:23 +0530

    feat: Implement login functionality with MUI components
    - Added @mui/material and @mui/icons-material dependencies for UI components.
    - Created a new Login component for user authentication.
    - Updated App component to conditionally render Login or welcome message based on login state.
    - Styled the Login component with custom CSS for better user experience.
    - Integrated API call for login functionality with error handling.

---
+++ src/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import ErrorIcon from "@mui/icons-material/Error";
+
+import "./Login.css";
+
+export default function Login({ setIsLoggedIn }) {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const jsonResponse = await response.json();
+    if (jsonResponse.success) {
+      setIsLoggedIn(true);
+    } else {
+      setError(jsonResponse.message);
+    }
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        {error && (
+          <Alert variant="outlined" severity="error" icon={false}>
+            {error}
+          </Alert>
+        )}
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <Button variant="contained" type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
